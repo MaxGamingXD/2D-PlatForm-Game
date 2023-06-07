d5c9cfb2c41883025e3ab1116df45d344cf5967e
MaxGaming <129042111+MaxGamingXD@users.noreply.github.com>
2023-06-07 20:21:55 +0930

    Finish Firing

--- 2D PlatForm Game/Assets/Scripts/PlayerAttack.cs
+++ 2D PlatForm Game/Assets/Scripts/PlayerAttack.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+
     private Animator anim;
     private PlayerMovement playerMovement;
     private float cooldownTimer = Mathf.Infinity;
@@ -27,5 +30,17 @@ public class PlayerAttack : MonoBehaviour
     {
         anim.SetTrigger("Attack");
         cooldownTimer = 0;
+
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 }
---
+++ 2D PlatForm Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float direction;
+    private bool hit;
+
+    private Animator anim;
+    private BoxCollider2D BoxCollider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        BoxCollider = GetComponent<BoxCollider2D>();
+    }
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        BoxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+    public void SetDirection(float _direction)
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        BoxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
